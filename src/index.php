aca51a9b565753270f965ac6833a0b770eacc630
sworgkh <sworgkh@gmail.com>
2018-07-15 09:14:28 +0300

    fixes from previous presentation

--- src/index.php
+++ src/index.php
@@ -22,10 +22,10 @@
                     <a class="nav-link" href="#"><img src="includes/images/improve.png"></a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <button class="fa fa-user fa-2x" type="submit"></button>
-                <button class="fa fa-cog fa-2x" type="submit"></button>
-            </form>
+            <div class="form-inline my-2 my-lg-0">
+                <button class="fa fa-user fa-2x" type="button"></button>
+                <button class="fa fa-cog fa-2x" type="button"></button>
+            </div>
         </div>
     </nav>
 
@@ -36,71 +36,72 @@
         <div class="container search-fix">
             <div class="input-group stylish-input-group">
                 <input type="text" class="form-control" placeholder="Search">
-                <button class="fa fa-search fa-2x" type="submit" onclick="addItemIndexPage()"></button>
+                <button class="fa fa-search fa-2x" type="submit" ></button>
             </div>
         </div>
     </nav>
+    <main>
+        <div class="container" id="addingEventsHere">
+            <div class="row col-title-row">
+                <div class="col-md-2">
+                    <p>Event Number</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Date Added</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Location</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Fix/Improve</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Confirmed by</p>
+                </div>
+                <div class="col-md-2">
+                    <p>Actions</p>
+                </div>
+            </div>
+            <?php
+            include("db.php");
 
-    <div class="container" id="addingEventsHere">
-        <div class="row col-title-row">
-            <div class="col-md-2">
-                <p>Event Number</p>
-            </div>
-            <div class="col-md-2">
-                <p>Date Added</p>
-            </div>
-            <div class="col-md-2">
-                <p>Location</p>
-            </div>
-            <div class="col-md-2">
-                <p>Fix/Improve</p>
-            </div>
-            <div class="col-md-2">
-                <p>Confirmed by</p>
-            </div>
-            <div class="col-md-2">
-                <p>Actions</p>
-            </div>
+            $eventsQuery = "Select * From events_208";
+            $eventsResult = mysqli_query($connection, $eventsQuery);
+            if (!$eventsResult) {
+                die("Select Events failed");
+            }
+            while ($event = mysqli_fetch_assoc($eventsResult)) {
+                echo
+                    "<div class=\"row one-event" . ($event["status"] == "InProcess" ? " in-progress" : "") . "\" id=\"event" . $event["id"] . "\">
+                <div class=\"col-md-2\">
+                    <p>" . $event["id"] . ($event["status"] == "InProcess" ? " <i class=\"fa fa-sync\"></i>" : "") . "</p>
+                </div>
+                <div class=\"col-md-2\">
+                    <p>" . date_format(new DateTime($event["date"]), "m/d/Y") . "</p>
+                </div>
+                <div class=\"col-md-2\">
+                    <p>" . $event["location"] . "</p>
+                </div>
+                <div class=\"col-md-2\">
+                    <p>" . $event["type"] . "</p>
+                </div>
+                <div class=\"col-md-2\">
+                    <p>" . $event["confirmed_count"] . "</p>
+                </div>
+                <div class=\"col-md-2\">
+                    <button class=\"icon-button fas fa-plus-square\"></button>
+                    <a href=\"eventDetailsPage.php?eventId=" . $event["id"] . "\" class=\"icon-button fas fa-clipboard-list\"></a>
+                    <button class=\"icon-button fas fa-check-circle\"></button>
+                    <button id=\"deleteEvent" . $event["id"] . "\" class=\"icon-button fas fa-trash-alt\"></button>
+                </div>
+            </div>";
+            }
+            mysqli_free_result($eventsResult);
+            mysqli_close($connection);
+            ?>
+
         </div>
-        <?php
-        include("db.php");
-
-        $eventsQuery = "Select * From events_208" ;
-        $eventsResult = mysqli_query($connection, $eventsQuery);
-        if (!$eventsResult) {
-            die("Select Events failed");
-        }
-        while ($event = mysqli_fetch_assoc($eventsResult)) {
-            echo
-                "<div class=\"row one-event".($event["status"]=="InProcess"?" in-progress":"")."\" id=\"event" . $event["id"] . "\">
-            <div class=\"col-md-2\">
-                <p>" . $event["id"] . ($event["status"]=="InProcess"?" <i class=\"fa fa-sync\"></i>":"")."</p>
-            </div>
-            <div class=\"col-md-2\">
-                <p>" . date_format(new DateTime($event["date"]), "m/d/Y") . "</p>
-            </div>
-            <div class=\"col-md-2\">
-                <p>" . $event["location"] . "</p>
-            </div>
-            <div class=\"col-md-2\">
-                <p>" . $event["type"] . "</p>
-            </div>
-            <div class=\"col-md-2\">
-                <p>" . $event["confirmed_count"] . "</p>
-            </div>
-            <div class=\"col-md-2\">
-                <button class=\"icon-button fas fa-plus-square\"></button>
-                <a href=\"eventDetailsPage.php?eventId=".$event["id"]."\" class=\"icon-button fas fa-clipboard-list\"></a>
-                <button class=\"icon-button fas fa-check-circle\"></button>
-                <button id=\"deleteEvent".$event["id"]."\" class=\"icon-button fas fa-trash-alt\"></button>
-            </div>
-        </div>";
-        }
-        mysqli_free_result($eventsResult);
-        mysqli_close($connection);
-        ?>
-
-    </div>
+    </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
